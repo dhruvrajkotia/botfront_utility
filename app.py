6eabd8eb4e2ec02e2a8088cc072c307eb2332659
Dhruv Rajkotia <dhruvrajkotia999@gmail.com>
2022-08-25 13:00:11 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,45 @@
+from flask import Flask, jsonify
+import os
+import logging
+import boto3
+from botocore.exceptions import ClientError
+import os
+from utils.config import load_config
+
+def upload_file(file_name, bucket, object_name=None):
+    if object_name is None:
+        object_name = os.path.basename(file_name)
+
+    s3_client = boto3.client('s3')
+    try:
+        with open(file_name, "rb") as f:
+            s3_client.upload_fileobj(f, bucket, object_name)
+    except ClientError as e:
+        logging.error(e)
+        return False
+    return True
+
+app = Flask(__name__)
+
+@app.route("/health-checkup")
+def health_checkup():
+    return jsonify({
+        'ok': True
+    })
+
+@app.route("/")
+def home():
+    return jsonify({
+        'ok': True
+    })
+
+@app.route("/sync_model")
+def sync_model():
+    status = upload_file(app.config.get('MODEL_FILE_PATH'),
+                         app.config.get('S3_BUCKET_NAME'))
+    return jsonify({'status': status})
+
+
+if __name__ == "__main__":
+    app = load_config()
+    app.run(host='0.0.0.0', debug=app.config.get('DEBUG'), port=app.config.get('PORT'))
---
+++ utils/config.py
@@ -0,0 +1,11 @@
+import os
+
+def load_config(): 
+    from app import app
+    app.config['MODEL_PATH'] = os.environ.get('MODEL_PATH')
+    app.config['S3_BUCKET_NAME'] = os.environ.get('S3_BUCKET_NAME')
+    app.config['PORT'] = os.environ.get('PORT', 8080)
+    app.config['DEBUG'] = True
+
+    return app
+
