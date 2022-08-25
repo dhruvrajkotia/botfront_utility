e7fea52bac42633a76fab3f58fbc994e14bde3de
Dhruv Rajkotia <dhruvrajkotia999@gmail.com>
2022-08-25 16:14:27 +0530

    Env var changes

--- utils/config.py
+++ utils/config.py
@@ -1,11 +1,13 @@
 import os
 
-def load_config(): 
+
+def load_config():
     from app import app
+    from dotenv import load_dotenv
+    load_dotenv()
     app.config['MODEL_PATH'] = os.environ.get('MODEL_PATH')
     app.config['S3_BUCKET_NAME'] = os.environ.get('S3_BUCKET_NAME')
     app.config['PORT'] = os.environ.get('PORT', 8080)
     app.config['DEBUG'] = True
 
     return app
-
